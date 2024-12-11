2e920b4dd066b7365e38c1b5bd5d08f8fa93eeee
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2024-12-11 15:50:47 +0700

    Create HealthRecord.java

---
+++ HealthRecord.java
@@ -0,0 +1,59 @@
+public class HealthRecord {
+    private int height;
+    private static final int MIN_PERMITTED_HEIGHT = 50;
+    private static final int MAX_PERMITTED_HEIGHT = 175;
+    private static final int DEFAULT_HEIGHT = 100;
+    private static int counter = 0;
+    private static double averageHeight = 0;
+
+    // TODO: 1 Think about the initial value for maximum and minimum height
+    private static int tallestHeight = MIN_PERMITTED_HEIGHT;// เพื่อ set ค่า limit ต้องกำหนดค่าเริ่มต้นที่สุดไว้ก่อน
+    private static int shortestHeight = MAX_PERMITTED_HEIGHT;
+
+    public HealthRecord(int height) {//constructor
+        setHeight(height);
+    }
+
+    public void setHeight(int height) {//setter
+        counter++;
+        // TODO: 2 Check if the input height falls within the permitted range
+        if (height > MIN_PERMITTED_HEIGHT && height < MAX_PERMITTED_HEIGHT)
+            this.height = height;
+        else
+            this.height = DEFAULT_HEIGHT;
+
+        // TODO: 3 Update the tallest and shortest height
+        if (this.height > tallestHeight)
+            tallestHeight = this.height;
+        if (this.height < shortestHeight)
+            shortestHeight = this.height;
+
+        if(counter > 0){
+            averageHeight = (averageHeight*(counter - 1) + this.height)/counter;
+        }
+    }
+    public static double getAverageHeight(){
+        return averageHeight;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public static int getTallestHeight() {
+       return tallestHeight;
+    }
+    public static int getShortestHeight() {
+        return shortestHeight;
+    }
+
+    public void displayDetails() {
+        System.out.println("Height (cm): " + getHeight());
+    }
+
+    public static void displayClassDetails() {
+        System.out.println("The tallest height (cm): " + getTallestHeight());
+        System.out.println("The shortest height (cm): " + getShortestHeight());
+        System.out.println("The average height (cm): " + getAverageHeight());
+    }
+}
